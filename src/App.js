195b5843ea10b1e7a237bf85c15c2154726913de
shafidark <safi29317@gmail.com>
2020-09-22 00:02:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import OtherAuth from './Components/OtherAuth/OtherAuth';
+import Booking from './Components/Booking/Booking';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import Hotel from './Components/Hotel/Hotel';
+
+export const userContext = createContext();
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: "",
+    email: "",
+    ConfirmPassword: "",
+    photoURL: "",
+    error: "",
+    success: false,
+    LoggedInUser: true,
+    newUser: false
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <userContext.Provider value={[user, setUser]}>
+      <Router>
+        <Switch>
+          <Route path="/home">
+          <Header/>
+          </Route>
+          <Route exact path="/">
+          <Header/>
+          </Route>
+          <Route path="/login">
+          <OtherAuth/>
+          </Route>
+          <Route path="/booking">
+          <Booking/>
+          </Route>
+          <PrivateRoute path="/hotel">
+            <Hotel/>
+          </PrivateRoute>
+        </Switch>
+      </Router>
+      
+    </userContext.Provider>
   );
 }
 
---
+++ src/Components/CreatAccount/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0LAOLqtGUTEtvR2o8W8cVnX-xUxurxdI",
+    authDomain: "travel-guruji-33f46.firebaseapp.com",
+    databaseURL: "https://travel-guruji-33f46.firebaseio.com",
+    projectId: "travel-guruji-33f46",
+    storageBucket: "travel-guruji-33f46.appspot.com",
+    messagingSenderId: "158829745946",
+    appId: "1:158829745946:web:f979b447ea73f196cc3ea3"
+  };
+
+  export default firebaseConfig;
